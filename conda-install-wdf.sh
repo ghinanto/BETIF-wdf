97fa464ad207691ccf66c6e4a94f4002fa3481be
ghinanto <antonioghin@tutanota.com>
2025-09-22 12:47:07 +0200

    Use active conda environment by deafult

--- conda-install-wdf.sh
+++ conda-install-wdf.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-##################################MANAGE_COMMANDLINE_ARGUMENTS#######################################
-#help function
+# Define the Help function
 Help()
 {
-   # Display Help
-   echo "Install wdf library (wdfpipe.github.io) through conda."
-   echo
-   echo "Syntax: conda-install-wdf.sh [-h/--help] CONDA_PREFIX_OF_YOUR_ENV"
-   echo "options:"
-   echo "-h/--help                 Print this Help."
-   echo "CONDA_PREFIX_OF_YOUR_ENV  Root directory of the conda env to install wdf into."
-   echo
+  # Display Help
+  echo "Install wdf library (wdfpipe.github.io) in the active conda environment."
+  echo
+  echo "Syntax: conda-install-wdf.sh [-h]"
+  echo "options:"
+  echo "h, --help        Print this Help."
+  echo
 }
+
 # Initialize a flag to track if an option was processed
 option_processed=0
+
 while [ $# -gt 0 ]; do
   case "$1" in
     -h|--help) # display Help
@@ -24,23 +24,35 @@ while [ $# -gt 0 ]; do
       echo "Error: Invalid option"
       exit 1
       ;;
-    *) # non-option argument
-      option_processed=1
-      # Check if the first non-option argument is a directory
-      if [ ! -d "$1" ]; then
-        echo "Error: '$1' is not a directory"
-        exit 1
-      fi
-      break
+    *) # unexpected argument
+      echo "Error: Unexpected argument '$1'"
+      exit 1
       ;;
   esac
   option_processed=1
   shift
 done
+
 # Check if no options or arguments were provided
 if [ $option_processed -eq 0 ]; then
-  Help
-  exit 0
+  # Check if inside a Conda environment
+  if [ -z "$CONDA_PREFIX" ]; then
+    echo "Error: Not inside a Conda environment (CONDA_PREFIX is not set)"
+    exit 1
+  fi
+  # Prompt user to install in CONDA_PREFIX
+  echo "Install wdf library in $CONDA_PREFIX? ([y]/n)"
+  read answer
+  case "$answer" in
+    n|N) # User chose not to install
+      echo "Installation aborted"
+      exit 0
+      ;;
+    y|Y|""|*) # User chose yes or pressed Enter (default)
+      export ENV_ROOT="$CONDA_PREFIX"
+      echo "ENV_ROOT set to $CONDA_PREFIX"
+      ;;
+  esac
 fi
 ##################################INSTALLING_WDF###################################################
 #update library cache
@@ -66,3 +78,20 @@ echo "WDF installation!"
 git clone https://gitlab.com/wdfpipe/wdf.git &&\
 cd wdf && python setup.py install &&\
 cd .. && rm -fr wdf/
+#check installation
+# Check if wdf can be imported
+"$ENV_ROOT/bin/python" -c "import wdf" 2>/dev/null
+if [ $? -eq 0 ]; then
+  echo "Library wdf is available"
+else
+  echo "Error: Cannot import wdf"
+  exit 1
+fi
+# Check if pytsa can be imported
+"$ENV_ROOT/bin/python" -c "import pytsa" 2>/dev/null
+if [ $? -eq 0 ]; then
+  echo "Library pytsa is available"
+else
+  echo "Error: Cannot import pytsa"
+  exit 1
+fi
